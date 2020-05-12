06f981e4fc3bcac7f21281075dfc298fd19e11ca
Bob Olde Hampsink <bob@robuust.digital>
2020-05-12 09:53:40 +0200

    Initial commit

---
+++ src/Module.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace robuust\heroku;
+
+use craft\events\RegisterComponentTypesEvent;
+use craft\helpers\App;
+use craft\services\Volumes;
+use robuust\heroku\volumes\Local;
+use yii\base\Event;
+
+/**
+ * Heroku module.
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * Initializes the module.
+     */
+    public function init()
+    {
+        parent::init();
+
+        Event::on(Volumes::class, Volumes::EVENT_REGISTER_VOLUME_TYPES, function (RegisterComponentTypesEvent $event) {
+            $event->types[] = Local::class;
+        });
+
+        $this->heroku();
+        $this->cloudcube();
+    }
+
+    /**
+     * Set heroku env.
+     */
+    private function heroku()
+    {
+        if (!($herokuAppName = App::env('HEROKU_APP_NAME'))) {
+            return;
+        }
+
+        $siteUrl = 'https://'.$herokuAppName.'.herokuapp.com';
+
+        // Adjust siteurl for Heroku PR apps
+        static::setEnv('CRAFT_SITEURL', $siteUrl);
+    }
+
+    /**
+     * Set cloudcube env.
+     */
+    private function cloudcube()
+    {
+        if (!($cloudcube = App::env('CLOUDCUBE_URL'))) {
+            return;
+        }
+
+        // Dissect cloudcube url
+        $components = parse_url($cloudcube);
+
+        // Get subfolder and host
+        $subfolder = substr($components['path'], 1);
+        $host = $components['scheme'].'://'.$components['host'];
+
+        // Set subfolder to env
+        static::setEnv('CLOUDCUBE_SUBFOLDER', $subfolder);
+
+        // Set host to env
+        static::setEnv('CLOUDCUBE_HOST', $host);
+    }
+
+    /**
+     * Set environment variables.
+     *
+     * @param string $key
+     * @param string $value
+     */
+    private static function setEnv(string $key, string $value)
+    {
+        $_ENV[$key] = $_SERVER[$key] = $value;
+        putenv($key.'='.$value);
+    }
+}
