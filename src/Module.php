bb938e73545f876f456fb2cd75a38fc60147fd3a
Bob Olde Hampsink <bob@robuust.digital>
2024-03-22 10:04:49 +0100

    Update cache before shutting down workers

--- src/Module.php
+++ src/Module.php
@@ -77,8 +77,8 @@ class Module extends \yii\base\Module
                 }
 
                 try {
-                    $client->patch('apps/'.$appName.'/formation/worker', ['quantity' => $quantity]);
                     Craft::$app->getCache()->set('currentDynos', $quantity);
+                    $client->patch('apps/'.$appName.'/formation/worker', ['quantity' => $quantity]);
                 } catch (\Exception $e) {
                     Craft::error($e->getMessage());
                 }
